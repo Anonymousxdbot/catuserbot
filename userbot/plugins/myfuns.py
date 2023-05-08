d797427c71ec445cdad36f2ade106492c6ce206d
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2023-05-08 10:16:38 +0530

    Update myfuns.py

--- userbot/plugins/myfuns.py
+++ userbot/plugins/myfuns.py
@@ -299,7 +299,59 @@ async def get_user(event):
             user_object = await event.client.get_entity(user)
             replied_user = await event.client(GetFullUserRequest(user_object.id))
 
+        except (TypeError, ValueError):async def get_user(event):
+
+    # Get the user from argument or replied message.
+
+    if event.reply_to_msg_id:
+
+        previous_message = await event.get_reply_message()
+
+        replied_user = await event.client(
+
+            GetFullUserRequest(previous_message.sender_id)
+
+        )
+
+    else:
+
+        user = event.pattern_match.group(1)
+
+        if user.isnumeric():
+
+            user = int(user)
+
+        if not user:
+
+            self_user = await event.client.get_me()
+
+            user = self_user.id
+
+        if event.message.entities:
+
+            probable_user_mention_entity = event.message.entities[0]
+
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+
+                user_id = probable_user_mention_entity.user_id
+
+                replied_user = await event.client(GetFullUserRequest(user_id))
+
+                return replied_user
+
+        try:
+
+            user_object = await event.client.get_entity(user)
+
+            replied_user = await event.client(GetFullUserRequest(user_object.id))
+
         except (TypeError, ValueError):
+
+            await event.edit("`I don't slap aliens, they ugly AF !!`")
+
+            return None
+
+    return replied_user
             await event.edit("`I don't slap aliens, they ugly AF !!`")
             return None
     return replied_user
